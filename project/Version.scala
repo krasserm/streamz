c36ab4d8e33fa4d813581d6878c93e2e87318296
Jakub Kozłowski <kubukoz@gmail.com>
2018-09-27 20:45:13 +0200

    Update fs2 to 1.0.0-RC1

--- project/Version.scala
+++ project/Version.scala
@@ -1,7 +1,7 @@
 object Version {
   val Akka = "2.5.16"
   val Camel = "2.20.2"
-  val Fs2 = "1.0.0-M5"
+  val Fs2 = "1.0.0-RC1"
   val Log4j = "2.10.0"
   val JUnitInterface = "0.11"
   val Scalatest = "3.0.5"
